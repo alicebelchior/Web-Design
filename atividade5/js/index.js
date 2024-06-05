95ef027b50a0bbcde9b74fe3327628c2893191c0
alicebelchior <alice.belchior23@gmail.com>
2024-06-04 23:43:01 -0300

    lixo de exercicio com lixo de video aula passada pelo arrombado do professor

---
+++ atividade5/js/index.js
@@ -0,0 +1,5 @@
+sessionStorage.descricao = document.getElementById("descricao").innerHTML;
+sessionStorage.valor = document.getElementById("valor").innerHTML;
+function resumoCompra() {
+  window.location = "resumo.html";
+}
